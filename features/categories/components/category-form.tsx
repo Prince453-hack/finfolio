6620df11ee201487a44bc58eee948dbb7fcb10b7
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-08-01 00:02:35 +0530

    category and categories api and ui

---
+++ features/categories/components/category-form.tsx
@@ -0,0 +1,87 @@
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { insertCategorySchema } from "@/db/schema";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Trash } from "lucide-react";
+
+const formSchema = insertCategorySchema.pick({ name: true });
+
+type FormValues = z.infer<typeof formSchema>;
+
+type Props = {
+  id?: string;
+  defaultValues?: FormValues;
+  onSubmit: (values: FormValues) => void;
+  onDelete?: () => void;
+  disabled?: boolean;
+};
+
+export const CategoryForm = ({
+  id,
+  defaultValues,
+  onSubmit,
+  onDelete,
+  disabled,
+}: Props) => {
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues,
+  });
+
+  const handleSubmit = (values: FormValues) => {
+    onSubmit(values);
+  };
+
+  const handleDelete = () => {
+    onDelete?.();
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleSubmit)}
+        className="space-y-4 pt-4"
+      >
+        <FormField
+          name="name"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input
+                  disabled={disabled}
+                  placeholder="e.g. Food, Travel, etc."
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button className="w-full" disabled={disabled}>
+          {id ? "Save Changes" : "Create Category"}
+        </Button>
+        {!!id && (
+          <Button
+            type="button"
+            disabled={disabled}
+            onClick={handleDelete}
+            className="w-full"
+            variant="outline"
+          >
+            <Trash className="size-4" /> <p className="ml-2">Delete Category</p>
+          </Button>
+        )}
+      </form>
+    </Form>
+  );
+};
--- features/categories/components/edit-category-sheet.tsx
+++ features/categories/components/edit-category-sheet.tsx
@@ -5,34 +5,34 @@ import {
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet";
-import { insertAccountSchema } from "@/db/schema";
-import { useConfirm } from "@/hooks/use-confirm";
-import { Loader2 } from "lucide-react";
+import { insertCategorySchema } from "@/db/schema";
 import { z } from "zod";
-import { useDeleteAccount } from "../api/use-delete-accounts";
-import { useEditAccount } from "../api/use-edit-accounts";
-import { useGetAccount } from "../api/use-get-account";
-import { useOpenAccount } from "../hooks/use-open-account";
-import { AccountForm } from "./account-form";
+import { useOpenCategory } from "../hooks/use-open-cateogry";
+import { CategoryForm } from "./category-form";
+import { useGetCategory } from "../api/use-get-cateogory";
+import { Loader2 } from "lucide-react";
+import { useEditCategories } from "../api/use-edit-category";
+import { useDeleteCategory } from "../api/use-delete-category";
+import { useConfirm } from "@/hooks/use-confirm";
 
-const formSchema = insertAccountSchema.pick({ name: true });
+const formSchema = insertCategorySchema.pick({ name: true });
 
 type FormValues = z.infer<typeof formSchema>;
 
-export const EditAccountSheet = () => {
-  const { isOpen, onClose, id } = useOpenAccount();
+export const EditCategorySheet = () => {
+  const { isOpen, onClose, id } = useOpenCategory();
   const [ConfirmDialog, confirm] = useConfirm(
     "Are you sure?",
-    "You are about to delete this transaction. This action cannot be undone."
+    "You are about to delete this category. This action cannot be undone."
   );
 
-  const accountQuery = useGetAccount(id);
-  const editMutation = useEditAccount(id);
-  const deleteMutation = useDeleteAccount(id);
+  const categoryQuery = useGetCategory(id);
+  const editMutation = useEditCategories(id);
+  const deleteMutation = useDeleteCategory(id);
 
   const isPending = editMutation.isPending || deleteMutation.isPending;
 
-  const isLoading = accountQuery.isLoading;
+  const isLoading = categoryQuery.isLoading;
 
   const onSubmit = (values: FormValues) => {
     editMutation.mutate(values, {
@@ -53,8 +53,8 @@ export const EditAccountSheet = () => {
     }
   };
 
-  const defaultValues = accountQuery.data
-    ? { name: accountQuery.data.name }
+  const defaultValues = categoryQuery.data
+    ? { name: categoryQuery.data.name }
     : { name: "" };
 
   return (
@@ -63,15 +63,15 @@ export const EditAccountSheet = () => {
       <Sheet open={isOpen} onOpenChange={onClose}>
         <SheetContent className="space-y-4">
           <SheetHeader>
-            <SheetTitle>Edit Account</SheetTitle>
-            <SheetDescription>Edit an existing account</SheetDescription>
+            <SheetTitle>Edit Category</SheetTitle>
+            <SheetDescription>Edit an existing category</SheetDescription>
           </SheetHeader>
           {isLoading ? (
             <div className="absolute inset-0 flex items-center justify-center">
               <Loader2 className="size-4 text-muted-foreground animate-spin" />
             </div>
           ) : (
-            <AccountForm
+            <CategoryForm
               id={id}
               onSubmit={onSubmit}
               disabled={isPending}
