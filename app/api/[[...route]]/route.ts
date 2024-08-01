8829fe7cb0457ce5d874f27076ede596dbd04e4c
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-08-01 19:50:38 +0530

    transactions api

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,12 +2,16 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import accounts from "./account";
 import categories from "./categories";
+import transactions from "./transactions";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-const routes = app.route("/accounts", accounts).route("categories", categories);
+const routes = app
+  .route("/accounts", accounts)
+  .route("categories", categories)
+  .route("transactions", transactions);
 
 export const GET = handle(app);
 export const POST = handle(app);
