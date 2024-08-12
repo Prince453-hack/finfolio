19386f29bba1ce874c8a334a56029f7235d180dc
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-08-12 17:44:10 +0530

    data card

---
+++ components/data-grid.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useGetSummary } from "@/features/summary/api/use-get-summary";
+import { formatDateRange } from "@/lib/utils";
+import { useSearchParams } from "next/navigation";
+import { FaPiggyBank } from "react-icons/fa";
+import { FaArrowTrendUp, FaArrowTrendDown } from "react-icons/fa6";
+import { DataCard } from "./data-card";
+
+export const DataGrid = () => {
+  const { data } = useGetSummary();
+  const params = useSearchParams();
+  const to = params.get("to") || undefined;
+  const from = params.get("from") || undefined;
+
+  const dateRange = formatDateRange({ from, to });
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8">
+      <DataCard
+        title="Remaining"
+        value={data?.remainingAmount}
+        percentageChange={data?.remainingChange}
+        icon={FaPiggyBank}
+        variant="default"
+        dateRange={dateRange}
+      />
+      <DataCard
+        title="Income"
+        value={data?.incomeAmount}
+        percentageChange={data?.incomeChange}
+        icon={FaArrowTrendUp}
+        variant="default"
+        dateRange={dateRange}
+      />
+      <DataCard
+        title="Expenses"
+        value={data?.expensesAmount}
+        percentageChange={data?.expensesChange}
+        icon={FaArrowTrendDown}
+        variant="default"
+        dateRange={dateRange}
+      />
+    </div>
+  );
+};
