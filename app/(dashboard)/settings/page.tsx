244f428ce8dacc6b774cd6c35b90fcf18baa004f
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-08-13 17:04:41 +0530

    settings page

---
+++ app/(dashboard)/settings/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Settings = () => {
+  return (
+    <div className="flex items-center justify-center mt-10">
+      <h1 className="text-3xl lg:text-4xl font-semibold animate-pulse">
+        Coming Soon...
+      </h1>
+    </div>
+  );
+};
+
+export default Settings;
