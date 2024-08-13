9e3748204e7bc4395f0eb945d62e9f77b432666d
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-08-13 13:27:00 +0530

    charts all type

---
+++ components/bar-variant.tsx
@@ -0,0 +1,42 @@
+import { format } from "date-fns";
+import {
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  Area,
+  BarChart,
+  CartesianGrid,
+  Bar,
+} from "recharts";
+import { CustomTooltip } from "./custom-tooltip";
+
+type Props = {
+  data:
+    | {
+        date: string;
+        income: number;
+        expenses: number;
+      }[]
+    | undefined;
+};
+
+export const BarVariant = ({ data }: Props) => {
+  return (
+    <ResponsiveContainer width="100%" height={350}>
+      <BarChart data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis
+          axisLine={false}
+          tickLine={false}
+          dataKey="date"
+          tickFormatter={(value) => format(value, "dd MMMM")}
+          style={{ fontSize: "12px" }}
+          tickMargin={16}
+        />
+        <Tooltip content={<CustomTooltip />} />
+        <Bar dataKey="income" fill="#3B82F6" className="drop-shadow-sm" />
+        <Bar dataKey="expenses" fill="#F43F5E" className="drop-shadow-sm" />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
